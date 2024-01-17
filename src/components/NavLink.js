e09d617a96e3f147a158efa3fe8b719cadebf992
klaenex <hello@cuozzovincenzo.be>
2024-01-17 10:35:37 +0100

    burger menu

--- src/components/NavLink.js
+++ src/components/NavLink.js
@@ -1,31 +1,38 @@
 import React from "react";
 
-const NavLink = () => {
+const NavLink = ({ isMenuOpen }) => {
+  const menuOpen = isMenuOpen
+    ? "mobile mobile--nav__open"
+    : "mobile mobile--nav__closed";
   return (
-    <ul className="list list--link">
-      <li>
-        <a href="../Presentation/">Présentation</a>
-      </li>
-      <li>
-        <a href="../education-permanente/">
-          Éducation Permanente & Cohésion Sociale
-        </a>
-        <ul className="list list--link">
-          <li>
-            <a href="../education-permanente/animations">Nos animations</a>
-          </li>
-          <li>
-            <a href="../education-permanente/formations">Nos formations</a>
-          </li>
-          <li>
-            <a href="../education-permanente/outils">Nos outils pédagogiques</a>
-          </li>
-        </ul>
-      </li>
-      <li>
-        <a href="../contact/">Contact</a>
-      </li>
-    </ul>
+    <div className={menuOpen}>
+      <ul className="list list--link">
+        <li>
+          <a href="../Presentation/">Présentation</a>
+        </li>
+        <li>
+          <a href="../education-permanente/">
+            Éducation Permanente & Cohésion Sociale
+          </a>
+          <ul className="list list--link">
+            <li>
+              <a href="../education-permanente/animations">Nos animations</a>
+            </li>
+            <li>
+              <a href="../education-permanente/formations">Nos formations</a>
+            </li>
+            <li>
+              <a href="../education-permanente/outils">
+                Nos outils pédagogiques
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href="../contact/">Contact</a>
+        </li>
+      </ul>
+    </div>
   );
 };
 
